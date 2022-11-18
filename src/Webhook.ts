663fadf6d6ff18145d7c8240c95e5d90b83e4e48
ApiDev234 <rockgaurishsethia@gmail.com>
2022-11-18 11:06:32 +0530

    Setup docs

--- src/Webhook.ts
+++ src/Webhook.ts
@@ -4,14 +4,27 @@ import phin from 'phin';
 import FormData from 'form-data';
 
 export class Webhook {
+  /* The default payload for the webhook */
   defaultPayload?: {};
+  /**
+   * Create a new Webhook instance
+   * @param {WebhookOptions} options
+   */
   constructor(private options: WebhookOptions) {}
 
-  public async get() {
+  /**
+   * Get webhook information
+   * @returns {APIWebhook}
+   */
+  public async get(): Promise<APIWebhook> {
     const data: APIWebhook = await this.request<APIWebhook>(this.options.url);
     return data;
   }
 
+  /**
+   * Set the default username for the webhook
+   * @param {string} username
+   */
   public setUsername(username: string) {
     this.defaultPayload = {
       ...this.defaultPayload,
@@ -19,6 +32,10 @@ export class Webhook {
     };
   }
 
+  /**
+   * Set the default avatar for the webhook
+   * @param {string} avatar
+   */
   public setAvatar(avatar: string) {
     this.defaultPayload = {
       ...this.defaultPayload,
@@ -26,14 +43,25 @@ export class Webhook {
     };
   }
 
+  /**
+   * Modify the webhook
+   * @param {WebhookOptions} options
+   */
   public async modify(options: WebhookOptions) {
     return this.request(this.options.url, 'PATCH', options);
   }
 
+  /**
+   * Delete the webhook
+   */
   public async delete() {
     return this.request(this.options.url, 'DELETE');
   }
 
+  /**
+   * Send a message to the webhook
+   * @param {ExecuteWebhookData} data
+   */
   public async execute(data: ExecuteWebhookData) {
     return this.defaultPayload
       ? this.request(
@@ -48,7 +76,16 @@ export class Webhook {
       : this.request(this.options.url, 'POST', data, data.files);
   }
 
-  public async request<T = any>(
+  /**
+   * Make a request to the webhook
+   * @param {string} url
+   * @param {string} method
+   * @param {any} postData
+   * @param {DiscordFile[] | DiscordFile} file
+   * @returns {Promise<any>}
+   * @private
+   */
+  private async request<T = any>(
     url: string,
     method?: string,
     postData?: any,
